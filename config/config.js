117dea4c230e7f755228300aaa3244b9f147cfb4
Marlon-Monroy <marlon@monroy.io>
2017-07-14 22:50:08 -0700

    machine change

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const bodyParser = require('body-parser')
+
+function setup(app) {
+    //TODO: need to setup app 
+}
+ module.exports.setup = setup
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const app = express()
+const config = require('./config/config')
+const port = process.env.port || 3000
 
+config.setup(app)
+app.listen(port, function(){
+    console.log('server started on ' + port)
+})
