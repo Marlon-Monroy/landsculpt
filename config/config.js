bf6bc8275c54b8c679aefdc041ad04ba88a58323
Marlon-Monroy <marlon@monroy.io>
2017-07-16 12:30:47 -0700

    config file from windows

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser')
 
 function setup(app) {
-    //TODO: need to setup app 
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
 }
  module.exports.setup = setup
